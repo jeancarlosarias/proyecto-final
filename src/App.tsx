477b94abe501573aa548651658a162b86358f1f0
Yunkaiz-dev <Vegarosariojoserafael@gmail.com>
2025-03-02 23:00:51 -0400

    Apps principales y sus estilos

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import LoginPage  from './Pages/MainPage/View/MainPage'
+
+
+const App: React.FC = () => {
+  
+
+  return (
+    <>
+      <LoginPage/>
+    </>
+  );
+};
+
+export default App;
