cc546f447ddb6c64dc49dcc7dd8302af08b9660a
Yunkaiz-dev <Vegarosariojoserafael@gmail.com>
2025-03-15 20:22:40 -0400

    Mejora del app, para solo llamar las rutas

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Rutas from './Components/rutas'; // Asegúrate de importar correctamente
 
+
 const App: React.FC = () => {
   return (
     <Rutas /> // Esto renderizará el enrutador y sus rutas
